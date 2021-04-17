0ac4ee32dbd3b451181b5e3ca660b7f9fd01e97e
Pratik Bhattacharya <pratikb@microsoft.com>
2021-04-18 04:23:07 +0530

    cleanup: removed orphan project files

--- src/service/Microsoft.UnifiedRedisPlatform.Service/Infrastructure/Telemetry/Client/IAppInsightsClientManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.UnifiedPlatform.Service.Common.Configuration;
-
-namespace Microsoft.UnifiedPlatform.Service.Telemetry.Client
-{
-    public interface IAppInsightsClientManager
-    {
-        IAppInsightsTelemetryClientWrapper CreateClient(ApplicationInsightsConfiguration applicationInsightsConfiguration, AppMetadataConfiguration appMetadataConfiguration);
-    }
-}
