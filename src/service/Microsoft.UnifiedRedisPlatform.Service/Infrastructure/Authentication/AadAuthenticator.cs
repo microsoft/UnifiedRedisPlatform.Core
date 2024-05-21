53d9bf62b0a2c48309121153a1f13c0e0ce0379c
Prasad Nikumbh <prnikumb@microsoft.com>
2024-05-21 20:35:57 +0530

    Migration of app to FIC ,upgraded the vulnerable packages and removed the ADAL references

--- src/service/Microsoft.UnifiedRedisPlatform.Service/Infrastructure/Authentication/AadAuthenticator.cs
+++ src/service/Microsoft.UnifiedRedisPlatform.Service/Infrastructure/Authentication/AadAuthenticator.cs
@@ -1,28 +1,74 @@
-﻿using System.Threading.Tasks;
-using System.Collections.Generic;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
+﻿using Microsoft.Identity.Client;
 using Microsoft.UnifiedPlatform.Service.Common.Authentication;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace Microsoft.UnifiedPlatform.Service.Authentication
 {
-    public class AadAuthenticator: IAuthenticator
+    public class AadAuthenticator : IAuthenticator
     {
         private readonly string _authority;
         private readonly string _clientId;
-        private readonly string _clientSecret;
-
-        public AadAuthenticator(string authority, string clientId, string clientSecret)
+        private readonly ConcurrentDictionary<string, IConfidentialClientApplication> confidentialApps = new ConcurrentDictionary<string, IConfidentialClientApplication>();
+        private readonly string _certificateThumprint;
+        public AadAuthenticator(string authority, string clientId, string certificateThumbprint)
         {
             _authority = authority;
             _clientId = clientId;
-            _clientSecret = clientSecret;
+            _certificateThumprint = certificateThumbprint;
         }
 
         public async Task<string> GenerateToken(string resourceId, Dictionary<string, string> additionalClaims)
         {
-            var authContext = new AuthenticationContext(_authority);
-            var authResult = await authContext.AcquireTokenAsync(resourceId, new ClientCredential(_clientId, _clientSecret));
+            IConfidentialClientApplication app = GetOrCreateConfidentialApp(_authority, _clientId);
+
+            var authResult = await app.AcquireTokenForClient(new[] { $"{resourceId}/.default" }).ExecuteAsync();
             return authResult.AccessToken;
+        }
+
+        private IConfidentialClientApplication GetOrCreateConfidentialApp(string authority, string clientId)
+        {
+            string confidentialAppCacheKey = CreateConfidentialAppCacheKey(authority, clientId);
+            if (confidentialApps.ContainsKey(confidentialAppCacheKey))
+            {
+                return confidentialApps[confidentialAppCacheKey];
+            }
+#if DEBUG
+            var certificate = GetCertificate(_certificateThumprint);
+
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder.Create(clientId)
+                                                                            .WithAuthority(AzureCloudInstance.AzurePublic, "microsoft.onmicrosoft.com")
+                                                                            .WithCertificate(certificate, true)
+                                                                            .Build();
+            confidentialApps.TryAdd(confidentialAppCacheKey, confidentialClientApplication);
+            return confidentialClientApplication;
+#else
+            IConfidentialClientApplication app =  ConfidentialClientApplicationBuilder
+                                                .Create(clientId)
+                                                .WithAuthority(new Uri(authority))
+                                                .WithClientAssertion(new ManagedIdentityClientAssertion(clientId).GetSignedAssertion).Build();
+             confidentialApps.TryAdd(confidentialAppCacheKey, app);
+             return app;
+#endif
+        }
+
+        private string CreateConfidentialAppCacheKey(string authority, string clientId)
+        {
+            return $"{authority}-{clientId}";
+        }
+
+        public X509Certificate2 GetCertificate(string certificateThumbprint)
+        {
+            using (var store = new X509Store(StoreName.Root, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+                var cert = store.Certificates.OfType<X509Certificate2>()
+                    .FirstOrDefault(x => x.Thumbprint?.ToUpper() == certificateThumbprint?.ToUpper());
+                return cert;
+            }
         }
     }
 }
